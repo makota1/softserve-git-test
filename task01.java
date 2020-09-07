3d02fa94ce9259905a9e8830550ce24da1e2befc
Alex Borovyk <maingroon@gmail.com>
2020-09-07 19:59:19 +0300

    Resolving problem

--- task01.java
+++ task01.java
@@ -12,9 +12,14 @@ class task01 {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello I'm mikiyur :)");
+        System.out.println("Hello I'm " + getCastenNick());
     }
 
     public static String getNickName() {
         return "makota";
+    }
+
+    public static String getCastenNick() {
+        return "Casten";
     }
 }
