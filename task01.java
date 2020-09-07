bc45592b9072aed456d8be24719abb04c261b2b5
admin <olexandr256@gmail.com>
2020-09-07 19:52:50 +0300

    метод для olexandr256

--- task01.java
+++ task01.java
@@ -9,7 +9,11 @@ class task01 {
         this.name = name;
     }
 
+    public String get_Olexandr256(){
+        return "olexandr256";
+    }
     public static void main(String[] args) {
         System.out.println("Hello git");
+
     }
 }
